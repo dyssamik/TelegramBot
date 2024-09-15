d9d0e9849cee21a20f1756d79cd7f22c071f79ca
anekryl <anekryl@PC>
2024-09-15 16:30:57 +0300

    Add project files.

---
+++ DbMethods.cs
@@ -0,0 +1,114 @@
+﻿using Npgsql;
+
+namespace TelegramBot
+{
+	class DbMethods
+	{
+		static async Task<NpgsqlConnection> OpenConnection()
+		{
+			string cs = "Host=127.0.0.1;Database=PW;User ID=postgres;Password=postgres;";
+			NpgsqlConnection conn = new NpgsqlConnection(cs);
+
+			try
+			{
+				await conn.OpenAsync();
+			}
+			catch (NpgsqlException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+
+			return conn;
+		}
+
+		public static async Task<int> GetUserId(long UserId)
+		{
+			NpgsqlConnection conn = await OpenConnection();
+
+			string cmdTxt = "SELECT user_id FROM users WHERE ext_id = " + UserId;
+			NpgsqlCommand cmd = new NpgsqlCommand(cmdTxt, conn);
+			NpgsqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+			int result = 0;
+			if (reader.HasRows)
+			{
+				await reader.ReadAsync();
+				result = (int)reader["user_id"];
+			}
+
+			return result;
+		}
+
+		public static async Task<string> SearchPetByName(string PetName)
+		{
+			NpgsqlConnection conn = await OpenConnection();
+
+			string cmdTxt = "SELECT pet_id FROM pets WHERE pet_name = '" + PetName + "'";
+			NpgsqlCommand cmd = new NpgsqlCommand(cmdTxt, conn);
+			NpgsqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+			string result = "";
+			if (reader.HasRows)
+			{
+				await reader.ReadAsync();
+				result = (string)reader["pet_name"];
+			}
+
+			return result;
+		}
+
+		public static async Task<int> ChangeUserRegState(long UserId)
+		{
+			NpgsqlConnection conn = await OpenConnection();
+
+			string cmdTxt = "UPDATE users SET reg_pending = TRUE WHERE ext_id = " + UserId;
+			NpgsqlCommand cmd = new NpgsqlCommand(cmdTxt, conn);
+			NpgsqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+			int result = reader.RecordsAffected;
+			return result;
+		}
+
+		public static async Task<int> RegisterUser(long UserId)
+		{
+			NpgsqlConnection conn = await OpenConnection();
+
+			string cmdTxt = "INSERT INTO users (ext_id, reg_pending, created_at) VALUES (" + UserId + ", TRUE, NOW()) ON CONFLICT (ext_id) DO NOTHING";
+			NpgsqlCommand cmd = new NpgsqlCommand(cmdTxt, conn);
+			NpgsqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+			int result = reader.RecordsAffected;
+			return result;
+		}
+
+		public static async Task<int> SetUserPet(long UserId, int PetId)
+		{
+			NpgsqlConnection conn = await OpenConnection();
+
+			string cmdTxt = "UPDATE users SET pet_id = " + PetId + ", reg_pending = FALSE WHERE ext_id = " + UserId + " AND reg_pending = TRUE";
+			NpgsqlCommand cmd = new NpgsqlCommand(cmdTxt, conn);
+			NpgsqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+			int result = reader.RecordsAffected;
+			return result;
+		}
+
+		public static async Task<string> GetUserPet(long UserId)
+		{
+			NpgsqlConnection conn = await OpenConnection();
+
+			string cmdTxt = "SELECT pets.pet_name AS pet_name FROM users LEFT JOIN pets ON users.pet_id = pets.pet_id WHERE users.ext_id = " + UserId;
+			NpgsqlCommand cmd = new NpgsqlCommand(cmdTxt, conn);
+			NpgsqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+			string result = "";
+			if (reader.HasRows)
+			{
+				await reader.ReadAsync();
+				result = (string)reader["pet_name"];
+			}
+
+			return result;
+		}
+	}
+}
---
+++ TelegramBot.cs
@@ -0,0 +1,213 @@
+﻿using System.Diagnostics;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TelegramBot
+{
+	class TelegramBot
+	{
+		static string Token { get; set; }
+		static string tokenFilePath = Environment.CurrentDirectory + "\\Token.txt";
+		static ITelegramBotClient botClient;
+		static CancellationTokenSource cts = new CancellationTokenSource();
+
+		static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+		{
+			try
+			{
+				if (update.Type == UpdateType.Message)
+				{
+					if (update.Message is not { } message)
+						return;
+
+					await OnMessage(message);
+				}
+
+				if (update.Type == UpdateType.CallbackQuery)
+				{
+					if (update.CallbackQuery is not { } query)
+						return;
+
+					await OnCallbackQuery(query);
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+			}
+		}
+
+		static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+		{
+			var ErrorMessage = exception switch
+			{
+				ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+				_ => exception.ToString()
+			};
+
+			Console.WriteLine(ErrorMessage);
+			return Task.CompletedTask;
+		}
+
+		static async Task OnMessage(Message message)
+		{
+			if (message.Text is not { } messageText) return;
+
+			if (message.From is not { } messageFrom) return;
+
+			messageText = messageText.Trim();
+
+			long userId = messageFrom.Id;
+			long chatId = message.Chat.Id;
+
+			Console.WriteLine($"Received a '{messageText}' message from {userId} in chat {chatId}.");
+
+			if (messageText.StartsWith('/'))
+			{
+				await OnCommand(messageText.ToLower(), chatId, userId);
+			}
+			else
+			{
+				await OnTextMessage(messageText, chatId, userId);
+			}
+		}
+
+		static async Task OnCommand(string messageText, long chatId, long userId)
+		{
+			switch (messageText)
+			{
+				case "/stop":
+					cts.Cancel();
+					Environment.Exit(0);
+					break;
+				case "/restart":
+					cts.Cancel();
+					Process.Start(Environment.ProcessPath!);
+					Environment.Exit(0);
+					break;
+			}
+
+			ReplyKeyboardMarkup replyKeyboardMarkup = new(new[]
+					{
+						new KeyboardButton[] { "🐈 Кошка", "🐕 Собака", "🦖 Динозавр", "🐸 Жаба" },
+					})
+			{
+				ResizeKeyboard = true,
+				OneTimeKeyboard = true
+			};
+
+			bool userAlreadyExists = await DbMethods.GetUserId(userId) > 0;
+
+			switch (messageText)
+			{
+				case "/start":
+					await botClient.SendTextMessageAsync(chatId, """
+                        /start - Показывает это сообщение
+                        /reg - Регистрирует Вас в боте
+                        /rereg - Перерегистрирует Вас в боте
+                        /info - Показывает информацию о вашем питомце
+                        /stop - Выключает бота
+                        /restart - Перезагружает бота
+                        """, cancellationToken: cts.Token);
+					break;
+				case "/reg":
+					if (userAlreadyExists) { await botClient.SendTextMessageAsync(chatId, "Вы уже зарегистрированы!", cancellationToken: cts.Token); break; }
+					await DbMethods.RegisterUser(userId);
+					await botClient.SendTextMessageAsync(chatId, "Выберите питомца:", replyMarkup: replyKeyboardMarkup, cancellationToken: cts.Token);
+					break;
+				case "/rereg":
+					if (!userAlreadyExists) { await botClient.SendTextMessageAsync(chatId, "Вы ещё не зарегистрированы!", cancellationToken: cts.Token); break; }
+					await DbMethods.ChangeUserRegState(userId);
+					await botClient.SendTextMessageAsync(chatId, "Выберите питомца:", replyMarkup: replyKeyboardMarkup, cancellationToken: cts.Token);
+					break;
+				case "/info":
+					string petName = await DbMethods.GetUserPet(userId);
+					await botClient.SendTextMessageAsync(chatId, $"Ваш питомец — {petName}", cancellationToken: cts.Token);
+					break;
+				default:
+					await botClient.SendTextMessageAsync(chatId, "Неизвестная команда", cancellationToken: cts.Token);
+					break;
+			}
+		}
+
+		static async Task OnTextMessage(string messageText, long chatId, long userId)
+		{
+			int petId;
+
+			switch (messageText)
+			{
+				case "🐈 Кошка":
+					petId = 1;
+					break;
+				case "🐕 Собака":
+					petId = 2;
+					break;
+				case "🦖 Динозавр":
+					petId = 3;
+					break;
+				case "🐸 Жаба":
+					petId = 4;
+					break;
+				default:
+					await botClient.SendTextMessageAsync(chatId, "Неизвестная команда", cancellationToken: cts.Token);
+					return;
+			}
+
+			int result = await DbMethods.SetUserPet(userId, petId);
+
+			if (result > 0)
+			{
+				await botClient.SendTextMessageAsync(chatId, "Успешная регистрация / перерегистрация!", replyMarkup: new ReplyKeyboardRemove(), cancellationToken: cts.Token);
+			}
+			else
+			{
+				await botClient.SendTextMessageAsync(chatId, "Вы ещё не зарегистрированы, либо не начат процесс перерегистрации.", cancellationToken: cts.Token);
+			}
+		}
+
+		static async Task OnCallbackQuery(CallbackQuery callbackQuery)
+		{
+			return;
+		}
+
+		static async Task Main(string[] args)
+		{
+			if (args.Length > 0)
+			{
+				Token = args[0];
+			}
+			else if (System.IO.File.Exists(tokenFilePath)
+				&& new FileInfo(tokenFilePath).Length != 0)
+			{
+				Token = System.IO.File.ReadAllText(tokenFilePath);
+			}
+			else
+			{
+				Console.WriteLine("No tg bot token was provided!");
+				Console.ReadKey();
+				return;
+			}
+
+			botClient = new TelegramBotClient(Token);
+
+			ReceiverOptions receiverOptions = new() { AllowedUpdates = { }, ThrowPendingUpdates = true };
+
+			botClient.StartReceiving(
+				updateHandler: HandleUpdateAsync,
+				pollingErrorHandler: HandleErrorAsync,
+				receiverOptions: receiverOptions,
+				cancellationToken: cts.Token);
+
+			User me = await botClient.GetMeAsync();
+
+			Console.WriteLine($"Start listening for @{me.Username}");
+			Console.ReadLine();
+
+			cts.Cancel();
+		}
+	}
+}
